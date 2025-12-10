ba0c2bd8c9360f2ec6a0c288320b1c261e666890
Zaikin Andrei <inerizal@gmail.com>
2025-12-10 17:05:25 +0100

    added test for route tracking

--- src/app/(tabs)/map/index.tsx
+++ src/app/(tabs)/map/index.tsx
@@ -1,16 +1,22 @@
 import Map from '@/src/components/Map';
+import { move } from '@/src/lib/mock';
 import { useUserStore } from "@/src/lib/store";
 import { StyleSheet } from "react-native";
 
+let is_moving = false;
 export default function Index() {
     const location = useUserStore((state) => state.user.location);
     const {start_loc, end_loc} = useUserStore.getState().get_cur_route();
+    if (!is_moving) {
+        is_moving = move();
+    }
     
     return (
         <Map 
             location={location?.coords} 
             start_loc={start_loc?.coords} 
-            end_loc={end_loc?.coords} 
+            end_loc={location?.coords} 
+            // end_loc={end_loc?.coords} 
         />
         
     );
